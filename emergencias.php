ccc7bee6493967ade35db2412b538c5eee2a0667
FveeTTV <fabrineira747@gmail.com>
2023-09-15 15:39:46 -0300

    base de datos

---
+++ emergencias.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.0
+ */
+
+/**
+ * Database `emergencias`
+ */
+
+/* `emergencias`.`ambulancia` */
+$ambulancia = array(
+);
+
+/* `emergencias`.`cargo` */
+$cargo = array(
+);
+
+/* `emergencias`.`colores` */
+$colores = array(
+);
+
+/* `emergencias`.`emergencia` */
+$emergencia = array(
+);
+
+/* `emergencias`.`login` */
+$login = array(
+);
+
+/* `emergencias`.`paciente` */
+$paciente = array(
+);
+
+/* `emergencias`.`turno` */
+$turno = array(
+);
+
+/* `emergencias`.`usuarios` */
+$usuarios = array(
+);
