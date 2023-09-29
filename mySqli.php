8207b3c234aca45344d99725ec7629b93340e2e2
FveeTTV <128607465+FveeTTV@users.noreply.github.com>
2023-09-29 17:14:54 -0300

    Add files via upload

---
+++ mySqli.php
@@ -0,0 +1,14 @@
+<div class="panel-body"> <?php   $conn = new mysqli("localhost", "root", "MI_PASSWORD", "MI-DB");      
+if ($conn->connect_error) 
+    {
+        die("ERROR: No se puede conectar al servidor: " . $conn->connect_error);
+    }  
+ 
+  echo 'Conectado a la base de datos.<br>'; 
+ 
+ 
+  echo "Numero de resultados: $result->num_rows"; 
+ 
+  $result->close(); 
+ 
+  $conn->close(); ?> </div> 
